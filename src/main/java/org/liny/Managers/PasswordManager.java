4b6865982e6c4ebca63ec774e05d155f31a4fbd7
4e6yPek48 <matchtek48@gmail.com>
2023-12-05 20:34:35 +0300

    и тут бы тоже ему руки оторвал

--- src/main/java/org/liny/Managers/PasswordManager.java
+++ src/main/java/org/liny/Managers/PasswordManager.java
@@ -11,12 +11,12 @@ import static org.liny.Main.*;
 public class PasswordManager {
 
 
-    public static @Nullable String getPassword(@NotNull UUID player) {
+    public static @Nullable String getPassword(@NotNull String player_name) {
 
         try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
              @NotNull PreparedStatement statement = connection.prepareStatement("SELECT password FROM passwords WHERE player_name = ?")) {
 
-            statement.setString(1, player.toString());
+            statement.setString(1, player_name);
 
             try (ResultSet resultSet = statement.executeQuery()) {
 
@@ -34,19 +34,19 @@ public class PasswordManager {
 
     }
 
-    public static void setPassword(@NotNull UUID player, @NotNull String pass) {
+    public static void setPassword(@NotNull String player_name, @NotNull String pass) {
 
         try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
              @NotNull PreparedStatement existStatement = connection.prepareStatement("SELECT * FROM passwords WHERE player_name = ?");
              @NotNull PreparedStatement insertStatement = connection.prepareStatement("INSERT INTO passwords (player_name, password) VALUES (?, ?)")) {
 
-            existStatement.setString(1, player.toString());
+            existStatement.setString(1, player_name.toString());
 
             ResultSet existResultSet = existStatement.executeQuery();
 
             if (existResultSet.next()) return;
 
-            insertStatement.setString(1, player.toString());
+            insertStatement.setString(1, player_name.toString());
             insertStatement.setString(2, pass);
             insertStatement.executeUpdate();
 
@@ -56,12 +56,12 @@ public class PasswordManager {
 
     }
 
-    public static void removePassword(@NotNull UUID player) {
+    public static void removePassword(@NotNull String player_name) {
 
         try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
              @NotNull PreparedStatement statement = connection.prepareStatement("DELETE FROM passwords WHERE player_name = ?")) {
 
-            statement.setString(1, player.toString());
+            statement.setString(1, player_name.toString());
 
             statement.executeUpdate();
 
