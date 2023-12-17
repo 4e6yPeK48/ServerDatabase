06ff63c23e2d09334fb757dd2589c81eb5b9f9b2
4e6yPek48 <matchtek48@gmail.com>
2023-12-17 19:40:20 +0300

    setClanField, getClanField, addClanField

--- src/main/java/org/liny/DataPacks/Clan.java
+++ src/main/java/org/liny/DataPacks/Clan.java
@@ -2,5 +2,5 @@ package org.liny.DataPacks;
 
 import org.jetbrains.annotations.NotNull;
 
-public record Clan(@NotNull String name, @NotNull String prefix, @NotNull String owner, @NotNull Integer level) {
+public record Clan(@NotNull String name, @NotNull String prefix, @NotNull String owner, @NotNull Integer level, Integer balance, Integer exp) {
 }
--- src/main/java/org/liny/Managers/ClanManager.java
+++ src/main/java/org/liny/Managers/ClanManager.java
@@ -10,6 +10,63 @@ import static org.liny.Main.*;
 
 public class ClanManager {
 
+    /*
+
+    ClanManager.setClanField("MyClan", "level", 5);
+
+    int clanLevel = ClanManager.getClanField("MyClan", "level");
+
+    ClanManager.addClanField("MyClan", "exp", 100);
+
+    */
+
+    public static void setClanField(@NotNull String clanName, @NotNull String fieldName, int value) {
+
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+
+             @NotNull PreparedStatement statement = connection.prepareStatement(
+                     "UPDATE clans SET " + fieldName + " = ? WHERE name = ?")) {
+
+            statement.setInt(1, value);
+            statement.setString(2, clanName);
+
+            statement.executeUpdate();
+
+        } catch (@NotNull SQLException ignored) {
+        }
+    }
+
+    public static int getClanField(@NotNull String clanName, @NotNull String fieldName) {
+
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+
+             @NotNull PreparedStatement statement = connection.prepareStatement(
+                     "SELECT " + fieldName + " FROM clans WHERE name = ?")) {
+
+            statement.setString(1, clanName);
+
+            try (@NotNull ResultSet resultSet = statement.executeQuery()) {
+
+                if (resultSet.next()) return resultSet.getInt(fieldName);
+
+            }
+
+        } catch (@NotNull SQLException ignored) {
+        }
+
+        return 0;
+
+    }
+
+    public static void addClanField(@NotNull String clanName, @NotNull String fieldName, Integer value) {
+
+        Integer currentValue = getClanField(clanName, fieldName);
+        Integer newValue = currentValue + value;
+
+        setClanField(clanName, fieldName, newValue);
+
+    }
+
     public static @NotNull Boolean clanExists(@NotNull String clanName) {
 
         try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
@@ -35,12 +92,14 @@ public class ClanManager {
         try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
 
              @NotNull PreparedStatement statement = connection.prepareStatement(
-                     "INSERT INTO clans (name, prefix, owner, level) VALUES (?, ?, ?, ?)")) {
+                     "INSERT INTO clans (name, prefix, owner, level, balance, exp) VALUES (?, ?, ?, ?, ?, ?)")) {
 
             statement.setString(1, clan.name());
             statement.setString(2, clan.prefix());
             statement.setString(3, clan.owner());
             statement.setInt(4, clan.level());
+            statement.setInt(5, clan.balance());
+            statement.setInt(6, clan.exp());
 
             statement.executeUpdate();
 
@@ -81,7 +140,9 @@ public class ClanManager {
                             resultSet.getString("name"),
                             resultSet.getString("prefix"),
                             resultSet.getString("owner"),
-                            resultSet.getInt("level")
+                            resultSet.getInt("level"),
+                            resultSet.getInt("balance"),
+                            resultSet.getInt("exp")
 
                     );
 
